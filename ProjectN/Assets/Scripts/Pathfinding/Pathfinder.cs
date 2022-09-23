87384f06ce2b58353c1b305a3e531c3ce655746d
BlackJeff <x>
2022-09-23 05:02:06 +0300

    Добавлен алгоритм поиска пути A*
    Пока что алгоритм игнорирует стены и прокладывает путь сквозь них
    Исправлена мелка ошибка в коде

---
+++ ProjectN/Assets/Scripts/Pathfinding/Pathfinder.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Pathfinder
+{
+    private const int MOVE_STRAIGHT_COST = 10;
+    private const int MOVE_DIAGONAL_COST = 14;
+
+    List<PathNode> openList;
+    List<PathNode> closedList;
+
+    public List<Vector3> FindPath(Vector3Int startPos, Vector3Int endPos, Tilemap tileMap)
+    {
+        PathNode startNode = new PathNode(startPos.x, startPos.y);
+        PathNode endNode = new PathNode(endPos.x, endPos.y);
+        openList = new List<PathNode> { startNode };
+        closedList = new List<PathNode>();
+
+        startNode.gCost = 0;
+        startNode.hCost = CalculateDistanceCost(startNode, endNode);
+        startNode.CalculateFCost();
+
+        while(openList.Count > 0)
+        {
+            PathNode currentNode = GetLowestFCostNode();
+            if (currentNode.x == endNode.x && currentNode.y == endNode.y)
+            {
+                return CalculatePath(currentNode, tileMap);
+            }
+            openList.Remove(currentNode);
+            closedList.Add(currentNode);
+
+            foreach (PathNode neighbourNode in GetNeighboursList(currentNode))
+            {
+                if (closedList.Contains(neighbourNode)) continue;
+
+                int tentativGCost = currentNode.gCost + CalculateDistanceCost(currentNode, neighbourNode);
+                if(tentativGCost < neighbourNode.gCost)
+                {
+                    neighbourNode.cameFromNode = currentNode;
+                    neighbourNode.gCost = tentativGCost;
+                    neighbourNode.hCost = CalculateDistanceCost(neighbourNode, endNode);
+                    neighbourNode.CalculateFCost();
+                }
+                if (!openList.Contains(neighbourNode)) openList.Add(neighbourNode);
+            }
+        }
+        return null;
+    }
+
+    private List<Vector3> CalculatePath(PathNode endNode, Tilemap tileMap)
+    {
+        List<Vector3> path = new List<Vector3>();
+        PathNode currentNode = endNode;
+        while(currentNode.cameFromNode != null)
+        {
+            path.Add(tileMap.GetCellCenterWorld(new Vector3Int(currentNode.x, currentNode.y, 0)));
+            currentNode = currentNode.cameFromNode;
+        }
+        path.Reverse();
+        return path;
+    }
+
+    private List<PathNode> GetNeighboursList(PathNode currentNode)
+    {
+        List<PathNode> neighbourList = new List<PathNode>();
+
+        //Left
+        
+        neighbourList.Add(PathNode.GetNode(currentNode.x - 1, currentNode.y));
+        //Left up
+        neighbourList.Add(PathNode.GetNode(currentNode.x - 1, currentNode.y + 1));
+        //Left down
+        neighbourList.Add(PathNode.GetNode(currentNode.x - 1, currentNode.y - 1));
+        //Right
+        neighbourList.Add(PathNode.GetNode(currentNode.x + 1, currentNode.y));
+        //Right up
+        neighbourList.Add(PathNode.GetNode(currentNode.x + 1, currentNode.y + 1));
+        //Right down
+        neighbourList.Add(PathNode.GetNode(currentNode.x + 1, currentNode.y - 1));
+        //Up
+        neighbourList.Add(PathNode.GetNode(currentNode.x, currentNode.y + 1));
+        //Down
+        neighbourList.Add(PathNode.GetNode(currentNode.x, currentNode.y - 1));
+
+        foreach(PathNode neighbourNode in neighbourList)
+        {
+            neighbourNode.gCost = int.MaxValue;
+        }
+
+        return neighbourList;
+    }
+    private PathNode GetLowestFCostNode()
+    {
+        PathNode lowestFCostNode = openList[0];
+        foreach(PathNode node in openList) 
+            if(lowestFCostNode.fCost > node.fCost) lowestFCostNode = node;
+        return lowestFCostNode;
+    }
+    private int CalculateDistanceCost(PathNode a, PathNode b)
+    {
+        int xDistance = Mathf.Abs(a.x - b.x);
+        int yDistance = Mathf.Abs(a.y - b.y);
+        int remaining = Mathf.Abs(xDistance - yDistance);
+        return MOVE_DIAGONAL_COST * Mathf.Min(xDistance, yDistance) + MOVE_STRAIGHT_COST * remaining;
+    }
+}
--- ProjectN/Assets/Scripts/PlayerUnitControl.cs
+++ ProjectN/Assets/Scripts/PlayerUnitControl.cs
@@ -5,17 +5,23 @@ using UnityEngine.Tilemaps;
 
 public class PlayerUnitControl : MobBase
 {
+    Pathfinder pathFinder = new Pathfinder();
     void Update()
     {
         UnitControl();
     }
     protected void UnitControl()
     {
-        if (Input.GetMouseButtonUp(0) && mapManager.isWalkable(Camera.main.ScreenToWorldPoint(Input.mousePosition)) && !isMoving)
+        if (Input.GetMouseButtonDown(0))
         {
             isMoving = true;
             Vector3Int cellPosition = tileMap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-            StartCoroutine(UnitMovement(PathCalc(cellPosition)));
+            List<Vector3> path = pathFinder.FindPath(cellPosition, unitPos, tileMap);
+            if (path != null)
+            {
+                for (int i = 0; i < path.Count - 1; i++)
+                    Debug.DrawLine(path[i], path[i + 1], Color.red, 2f);
+            }
         }
     }
 }
